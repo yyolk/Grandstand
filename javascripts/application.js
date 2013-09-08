11540a0b800bfe92cacec5d7e86b976aef18cfe0
Andrew Liebchen <aliebchen@gofdn.com>
2013-09-07 23:07:56 -0400

    Cleanup

--- javascripts/application.js
+++ javascripts/application.js
@@ -43,7 +43,6 @@ $(document).ready(function() {
       data: {type: type},
       context: document.body,
       success: function(data, textStatus, xhr) {
-        
         if (data.length) {
           for (var j=0; j < data.length; j++) {
             var scoreClass = data[j].score;
@@ -52,20 +51,22 @@ $(document).ready(function() {
             }; 
             if(!$.isEmptyObject(data[j]['assigned_to'])){
               userId = 'user-' + data[j]['assigned_to']['id'];
+            } else {
+              userId = 'unassigned'
             }
-            html += '<a href="' + data[j].short_url + '"  class="item ' + scoreClass + ' ' + data[j]['type'] + ' ' + userId + ' ' + data[j].status + '" data-item-id="' + data[j].number + '" target="_blank">';
+            html += '<a href="' + data[j].short_url + '"  class="item ' + scoreClass + ' ' + data[j]['type'] + ' ' + userId + ' ' + data[j].status + ' target="_blank">';
             html += '<div><strong>#' + data[j].number + '</strong> ';
             html += '<span class="title">' + data[j].title + '</span></div>';
             html += '</a>';
           };
         } else {
-          html = '<div><div>No items</div></div>';
+          html = '<div class="missing-data">No items</div>';
         };
         $('#items').html(html);
         initIsotope(2000);
       },
       error: function(xhr, textStatus, errorThrown) {
-        $('#' + type).html('');
+        $('#items').html('<div class="missing-data">Error</div>');
       }
     });
   };
@@ -75,14 +76,12 @@ $(document).ready(function() {
     type: 'GET',
     dataType: 'json',
     complete: function(xhr, textStatus) {
-      //called when complete
+      // Called when complete
     },
     success: function(data, textStatus, xhr) {
       var html = '<a class="user filter all active" data-filter=".item">All users</a>'; // All user
       for (var i=0; i < data.length; i++) {
-        if (data[i].revoked) {
-
-        } else {
+        if (!data[i].revoked) {
           html += '<li role="menuitem">';
           html += '<a class="user filter" data-filter=".user-'+data[i].id+'">';
           html += data[i].first_name + ' ' + data[i].last_name;
@@ -93,7 +92,7 @@ $(document).ready(function() {
       $('#users .dropdown-menu').html(html);
     },
     error: function(xhr, textStatus, errorThrown) {
-      //called when there is an error
+      // Called when there is an error
     }
   });
 
