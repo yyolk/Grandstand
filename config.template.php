c89737cbc587f3073a57e38b19dbcf09ad90018e
Andrew Liebchen <aliebchen@gofdn.com>
2013-09-08 11:31:24 -0400

    Add config.template

---
+++ config.template.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = array();
+
+$config['sprintly'] = array();
+$config['sprintly']['api_username'] = '';
+$config['sprintly']['api_key'] = '';
+$config['sprintly']['product_id'] = '';
+$config['sprintly']['product_name'] = '';
